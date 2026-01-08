48dada1810f268099e79c65bf92e7272a68b966d
Tom Weerheijm <tomweerheijm@proton.me>
2026-01-08 13:27:03 +0100

    Slightly more model

--- simulation/model.py
+++ simulation/model.py
@@ -9,11 +9,11 @@ class Railroad(mesa.Model):
 
     def __init__(self, length):
         super().__init__()
-        self.rails = Rails(length)
-        Trains = Train.create_agents(self, 2, rails)
+        self.rails = Rails(10000)
+        Trains = Train.create_agents(self, 2, rails, [0, 3000], [100, 3100], [200, 100])
         Signals = Signal.create_agents(self, 20, rails)
         
 
     def step(self):
         """Advance the model by one step."""
-        self.Trains.shuffle_do("move")
+        self.Trains.shuffle_do("step")
