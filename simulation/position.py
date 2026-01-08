de724f93ac7d11980c4c9bf2e90008eff4e822f7
Silvana Klaver <silvana.klaver@student.uva.nl>
2026-01-08 12:59:09 +0100

    Train basis

--- simulation/position.py
+++ simulation/position.py
@@ -1,14 +1,13 @@
-
 class Position:
     """
-        Contains the position of an object with length. 
+        Contains the position of an object with length.
         Oriented from the start of the line to the end of the line. (So the start of a train is at the back.)
     """
     def __init__(self, start, end):
         assert(begin < end)
         self.start = start
         self.end = end
-    
+
     def __add__(self, other):
         assert(isinstance(other, float))
         self.start += other
