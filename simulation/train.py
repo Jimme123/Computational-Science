b005b865ac086b1c60292b59eee27193663186a8
Silvana Klaver <silvana.klaver@student.uva.nl>
2026-01-08 12:10:26 +0100

    train start

--- simulation/train.py
+++ simulation/train.py
@@ -1,3 +1,17 @@
-class Train(mesa.Agent):
-    def __init__(self, model):
-        super().__init__(model)
+class Train(PositionalAgent):
+    def __init__(self, model, position, speed):
+        super().__init__(model, position)
+        self.speed = speed
+
+    def move(self, direction):
+        next_position = self.position + self.speed * direction
+
+        signal = self.model.rails.next_signal(next_position)
+
+        if signal == "RED":
+            self.speed = 0
+        else:
+            self.position = next_position
+
+    def step(self):
+        self.move(direction = 1)
