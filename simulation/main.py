57fbf31b8f897e186291292e4290a80ff47b190f
Tom Weerheijm <tomweerheijm@proton.me>
2026-01-12 11:08:06 +0100

    Did static blocks

--- simulation/main.py
+++ simulation/main.py
@@ -1,5 +1,6 @@
 from model import *
+from blockSignalling import *
 
-railroad = Railroad(10000)
+railroad = Railroad(10000, BlockSignalling)
 for i in range(500):
     railroad.step()
--- simulation/model.py
+++ simulation/model.py
@@ -1,18 +1,14 @@
 import mesa
 
 from block import *
-from signallingControl import *
 from train import *
 
 class Railroad(mesa.Model):
     """The model containing the trains and signals."""
 
-    def __init__(self, length, type):
+    def __init__(self, length, signalling_control_class):
         super().__init__()
-        self.signalling_control = SignallingControl(self, 10000)
-        self.trains = Train.create_agents(self, 2, [Position(0, 100), Position(3000, 3100)], self.signalling_control, [55, 25], 1.1, -1.3)
-        n = 20
-        self.blocks = Block.create_agents(self, n, [Position(i * length / n, (i + 1) * length / n) for i in range(n)], self.signalling_control)
+        self.signalling_control = signalling_control_class(self, 10000)
         
 
     def step(self):
@@ -21,4 +17,10 @@ class Railroad(mesa.Model):
         self.trains.shuffle_do("step")
         for train in self.trains:
             print(train)
+    
+    def add_train(*args):
+        Train.create_agents(self, 1, *args)
+
+    def add_block(*args):
+        Block.create_agents(self, 1, *args)
  
