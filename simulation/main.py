e4660b459289ac55543b20b2b08611d98d8b6cfe
Tom Weerheijm <tomweerheijm@proton.me>
2026-01-12 11:49:37 +0100

    round when printing

--- simulation/main.py
+++ simulation/main.py
@@ -2,11 +2,11 @@ from model import *
 from staticBlockSignalling import *
 from position import *
 
-length = 10000
+length = 10500
 railroad = Railroad(length, StaticBlockSignalling)
 railroad.add_train(Position(0, 100), 55, 1.3, -1.1)
 railroad.add_train(Position(3000, 3100), 25, 1.3, -1.1)
-n = 5
+n = 7
 for i in range(n):
     railroad.add_block(Position(i * length / n, (i + 1) * length / n))
 
