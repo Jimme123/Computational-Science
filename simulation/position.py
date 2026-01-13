7eb1e9d38368e73c7c607e5dcf82c8fa1c08da06
A056G <arne.goudzwaard@student.uva.nl>
2026-01-13 14:46:14 +0100

    fixxed overlap

--- simulation/position.py
+++ simulation/position.py
@@ -24,12 +24,23 @@ class Position:
         return (self.start, self.end)
 
 
+def get_segments(position):
+    start, end = position.bounds
+    if end > start:
+        return [(start, end)]
+    else:
+        return [(start, position.rail_length), (0, end)]
+
+
 def overlap(position_a, position_b):
     """
     Determines if the positions a and b have overlap when in a circle track
     """
-    start_a, end_a = position_a.bounds
-    start_b, end_b = position_b.bounds
+    #start_a, end_a = position_a.bounds
+    #start_b, end_b = position_b.bounds
+    segments_a = get_segments(position_a)
+    segments_b = get_segments(position_b)
+    """
     if start_a > end_a and start_b > end_b:
         return True
     if start_a > end_a or start_b > end_b:
@@ -41,10 +52,13 @@ def overlap(position_a, position_b):
             return True
         else:
             return False
-    if start_a <= start_b <= end_a or\
-        start_a <= end_b <= end_a or \
-        (start_b <= start_a and end_a <= end_b):
-        return True
+    """
+    for start_a, end_a in segments_a:
+        for start_b, end_b in segments_b:
+            if start_a <= start_b <= end_a or\
+                start_a <= end_b <= end_a or \
+                (start_b <= start_a and end_a <= end_b):
+                return True
     return False
 
 
