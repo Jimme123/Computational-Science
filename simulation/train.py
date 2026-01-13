bb1d181b9dad4d7f6dcfbb482de9309358dc42d4
Tom Weerheijm <tomweerheijm@proton.me>
2026-01-13 15:11:15 +0100

    added train logic from station to modular logic

--- simulation/train.py
+++ simulation/train.py
@@ -19,27 +19,52 @@ class Train(PositionalAgent):
     def step(self):
         signal, distance = self.signalling_control.next_signal(self)
 
-        if signal == Color.RED:
-            acceleration = self.braking if (self.brake_distance(0) > distance - 20) else 0
-        elif signal == Color.ORANGE:
-            acceleration = self.braking if self.brake_distance(self.max_speed / 2) > distance - 20 else (self.acceleration if self.speed < self.max_speed else 0)
+        if signal != Color.STATION:
+            self._wait = None
         else:
-            acceleration = self.acceleration if self.speed < self.max_speed else 0
+            if self.speed == 0:
+                self._wait = self._wait - dt if self._wait is not None else 10
+            
+            if self._wait is not None and self._wait <= 0:
+                signal = Color.ORANGE
+            else:
+                signal = Color.RED
 
-        self.speed += acceleration * dt
-        if self.speed < 0:
-            self.speed = 0
+        if signal == Color.RED:
+            # Go half speed and stop 20m before the signal
+            if self.brake_distance(0) > distance - 20:
+                self.speed = max(0, self.speed - self.braking * dt)
+            elif self.speed > self.max_speed / 2:
+                self.speed = max(self.max_speed / 2, self.speed - self.braking * dt)
+            elif self.speed < self.max_speed / 2:
+                self.speed = min(self.max_speed / 2, self.speed + self.acceleration * dt)
+
+        if signal == Color.ORANGE:
+            # Go maximal speed as long as possible, but pass the signal at half speed
+            if self.brake_distance(self.max_speed / 2) > distance:
+                self.speed = max(self.max_speed / 2, self.speed - self.braking * dt)
+            elif self.speed > self.max_speed:
+                self.speed = max(self.max_speed, self.speed - self.braking * dt)
+            elif self.speed < self.max_speed:
+                self.speed = min(self.max_speed, self.speed + self.acceleration * dt)
+        
+        if signal == Color.GREEN:
+            # Full steam ahead
+            self.speed = min(self.max_speed, self.speed + self.acceleration * dt)
+
+
         self.position += self.speed * dt
 
-        # if self.position.start >= self.signalling_control.length:
-        #    self.signalling_control.remove_train(self)
-        #    super().remove()
+        if self.position.start >= self.signalling_control.length:
+            self.signalling_control.remove_train(self)
+            super().remove()
 
     def brake_distance(self, speed):
-        return (self.speed - speed)**2 / 2*-self.braking if self.speed > speed else 0
+        return (self.speed - speed)**2 / 2*self.braking if self.speed > speed else 0
 
     def __str__(self):
-        return f"{self.position}, speed: {self.speed:.1f}, next signal: {self.signalling_control.next_signal(self)}"
+        signal, distance = self.signalling_control.next_signal(self)
+        return f"{self.position}, speed: {self.speed:.1f}, next signal: {signal} in {distance:.0f}"
 
 #    def get_acceleration(self, tractive_force):
 #        if self.speed == self.max_speed:
