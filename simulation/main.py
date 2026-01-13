160fc4a5aa7b2cc414385465f4eb7cea32c70483
Tom Weerheijm <tomweerheijm@proton.me>
2026-01-13 14:57:37 +0100

    changed names in main.py to reflect visualize.py

--- simulation/main.py
+++ simulation/main.py
@@ -4,19 +4,19 @@ from model import *
 from staticBlockSignalling import *
 from position import *
 
-length = 10500
-railroad = Railroad(length, StaticBlockSignalling)
-railroad.add_train(Position(0, 100), 55, 1.3, 1.1)
-railroad.add_train(Position(3000, 3100), 25, 1.3, 1.1)
-n = 7
-positions = np.linspace(0, length, n + 1)
-stations = [2, 6]
+rail_length = 12000
+model = Railroad(rail_length, StaticBlockSignalling)
+model.add_train(Position(0, 100), 55, 1.3, 1.1)
+model.add_train(Position(3000, 3100), 25, 1.3, 1.1)
+n = 8
+positions = np.linspace(0, rail_length, n + 1)
+stations = [2, 4]
 for i in range(n):
     if i in stations:
-        railroad.add_station(Position(positions[i], positions[i]+1))
-        railroad.add_block(Position(positions[i]+1, positions[i+1]))
+        model.add_station(Position(positions[i], positions[i]+10))
+        model.add_block(Position(positions[i]+10, positions[i+1]))
     else:
-        railroad.add_block(Position(positions[i], positions[i+1]))
+        model.add_block(Position(positions[i], positions[i+1]))
 
 for i in range(400):
-    railroad.step()
+    model.step()
