45d00c2a4a9798944ada19daa0a6e8abb2cff545
A056G <arne.goudzwaard@student.uva.nl>
2026-01-14 15:40:02 +0100

    begintje aan model setup

--- analysis/model_setup.py
+++ analysis/model_setup.py
@@ -1,1 +1,15 @@
 from simulation.model import Railroad
+from simulation.staticBlockSignalling import *
+from simulation.movingBlockSignalling import *
+from simulation.position import *
+
+rail_length = 3000
+signalling_class = StaticBlockSignalling
+sight = 300
+dt = 1
+wait_time = 10
+model = Railroad(rail_length, signalling_class, sight, dt, wait_time)
+metro_length = 108.68
+metro_specifications = (19.4444444, 1.27, 1.35)
+block_size = 200  # 156
+
