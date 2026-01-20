cb3bb155c1e8df05126fa590db1786502a0749ee
Jimme123 <jimme.hotho@student.uva.nl>
2026-01-20 13:41:46 +0100

    better parameters

--- analysis/main.py
+++ analysis/main.py
@@ -10,10 +10,13 @@ from visualize import *
 from osloMetro import *
 from generateModel import *
 
-model = generate_model(distances_variation=1)
+model = generate_model(signalling_type="moving", 
+                       distances_variation=1, 
+                       acc_dist=[0.25, 0.5, 0.25], 
+                       braking_dist=[0.25, 0.5, 0.25])
 # print(test_capacity(model, metro_specifications, max_trains=30, verbose=True))
 
 for i in range(3000):
     model.step()
 
-#visualize(model, 1000)
+visualize(model, 1000)
