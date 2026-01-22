03a8f7daeaaa939057666d9f8b978f8cf8d40b57
Jimme123 <jimme.hotho@student.uva.nl>
2026-01-22 13:37:03 +0100

    Oslo test fully done

--- analysis/Oslo_test.py
+++ analysis/Oslo_test.py
@@ -6,7 +6,7 @@ import numpy as np
 
 static_model = osloMetro.generate_metro("static", station_size=0)
 moving_model = osloMetro.generate_metro("moving")
-result = test_capacity([static_model, moving_model], [osloMetro.metro_specifications], max_trains=20, repetitions=1, verbose=True)
+result = test_capacity([static_model, moving_model], [osloMetro.metro_specifications], max_trains=20, repetitions=1, verbose=False)
 
 static_capacities = []
 moving_capacities = []
@@ -18,22 +18,21 @@ for entry in result:
     static_capacities.append(static)
     moving_capacities.append(moving)
 
+reference_max_static = 28
+reference_max_moving = 40
 max_static = max(static_capacities)
 max_moving = max(moving_capacities)
-oslo_max_static = 28
-oslo_max_moving = 40
 
 print("Max static:", max_static)
 print("Max moving:", max_moving)
 
 
-
-groups = ['Oslo', 'Simulatie']
-static_values = [oslo_max_static, max_static]
-moving_values = [oslo_max_moving, max_moving]
+groups = ['Reference', 'Our model']
+static_values = [reference_max_static, max_static]
+moving_values = [reference_max_moving, max_moving]
 width = 0.2
 in_group_spacing = 0.05
-group_spacing = 1
+group_spacing = 0.8
 
 x = np.arange(len(groups)) 
 plt.figure()
@@ -45,4 +44,5 @@ plt.xticks(x, groups)
 plt.title('Vergelijking Oslo vs Simulatie')
 plt.legend()
 
+plt.savefig("../plots/Oslo_plot.png", bbox_inches="tight", dpi=300)
 plt.show()
